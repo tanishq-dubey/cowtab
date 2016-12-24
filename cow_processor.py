bab5256e82fd4fb22070e3b532604296d6d2abf0
Jesse Li <jessejesse123@gmail.com>
2016-12-24 11:13:02 -0500

    tab completion

--- cow_processor.py
+++ cow_processor.py
@@ -8,6 +8,7 @@ RENDERED_PATH = "./cowfiles/"
 for cowfile in os.listdir(COWPATH):
     try:
         cow_name = cowfile.split(".cow")[0]
+        print(cow_name)
         cow = subprocess.check_output(["cowsay", "-f", cow_name, "cow processing"])
 
         with open(os.path.join(RENDERED_PATH, cowfile), "wb+") as f:
