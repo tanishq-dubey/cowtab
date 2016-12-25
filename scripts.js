122877e3e8f6b48a012300ced968ffa8846952c6
metarmask <ihvarfner@gmail.com>
2016-12-25 22:55:27 +0100

    Refactor new tab and fix some issues
    - Move list of fortune files to file
    - Use defer attribute instead of DOMContentLoaded event
    - Use flexbox for centering
    - Replace loops with array methods
    - Replace innerHTML and HTML escaping with textContent
    - Replace $.ajax with fetch and async/await
    - Remove jQM
    - Fix tabs being counted as one character
    - Fix extra space on cow
    - Fix inconsistent spacing
    
    Libraries that are used for only a couple of things on
    a page that is going to be loaded many, many times
    is unneccessary.
    
    Because we are in a Chrome extension environment we
    can use the latest and greatest features without
    transpiling, let's use that opportunity! (flexbox,
    fetch, async/await, array methods)

--- scripts.js
+++ scripts.js
@@ -1,76 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var lines;
-    var randomN;
+const speechElement = document.querySelector("#speech");
+const tabSize = 8;
+const getRandomIn = array => {
+	return array[Math.floor(Math.random() * array.length)];
+}
 
-    var files = ["art", "ascii-art", "computers", "cookie", "definitions", "drugs", "education", "ethnic", "food", "fortunes", "goedel", "humorists", "kids", "law", "linuxcookie", "literature", "love", "magic", "medicine", "men-women", "miscellaneous", "news", "people", "pets", "platitudes", "politics", "riddles", "science", "songs-poems", "sports", "startrek", "translate-me", "wisdom", "work", "zippy"];
-    var randF = parseInt(Math.random() * files.length);
-    $.ajax({
-        url: 'fortunes/' + files[randF]
-    }).done(function(content){
-        lines = content.split(/\n%\n/);
-        if(lines && lines.length) {
-            do {
-                randomN = parseInt(Math.random() * lines.length);
-                var cowsayRun = document.getElementById("cowsay-run");
-                var cowsayOutput = document.getElementById("cowsay-output");
-                var cowsayTop = document.getElementById("cowsay-barsTop");
-                var cowsayBot = document.getElementById("cowsay-barsBot");
-                var inputText = lines[randomN];
-                var barsTop = "";
-                var barsBot = "";
-                var textArray = inputText.split('\n');
-                var setText = "";
-                var maxWidth = 0;
-                var numLines = 0;
+(async () => {
+	const fortuneFiles = (await (await fetch("fortunes/index.txt")).text()).split("\n").slice(0, -1);
+	const fortuneFileContent = await (await fetch("fortunes/" + getRandomIn(fortuneFiles))).text();
+	const fortunes = fortuneFileContent.split("\n%\n")
+	.filter(fortune => fortune.length !== 0);
 
-                for(var i = 0; i < textArray.length; ++i) {
-                    textArray[i].replace('  ', ' ');
-                    if(textArray[i].length === 0) {
-                        console.log("FOUND ZERO LENGTH");
-                    }
-                }
+	let fortuneLines = getRandomIn(fortunes).split("\n");
 
-                for(var i = 0; i < textArray.length; ++i) {
-                    if(maxWidth < textArray[i].length) {
-                        maxWidth = textArray[i].length;
-                    }
-                }
+	// Replace tabs with spaces so length checks work
+	fortuneLines = fortuneLines.map(line => {
+		return line.replace(/\t/g, " ".repeat(tabSize));
+	});
 
-                // If only one line, clean and then add brackets
-                if (textArray.length === 1) {
-                    //inputText[0] = inputText[0].substring(0, inputText[0].length-1)
-                    maxWidth = textArray[0].length;
-                    textArray[0] = " &#60;" + textArray[0] + "&#62;";
-                } else {
-                    // Otherwise, build a cool little bubble
-                    textArray[0] = "/ " + textArray[0] + new Array(maxWidth - textArray[0].length + 1).join(' ') + " \\\n";
-                    for (var i = 1; i < textArray.length - 1; ++i) {
-                        textArray[i] = "| " + textArray[i] + new Array(maxWidth - textArray[i].length + 1).join(' ') + " |\n";
-                    }
-                    try {
-                        textArray[textArray.length -1] = "\\ " + textArray[textArray.length -1] + new Array(maxWidth - textArray[textArray.length -1].length + 1).join(' ') + " /";
-                    } catch(err) {
-                        console.log("ERROR!!!");
-                    }
+	const maxWidth = fortuneLines.reduce((max, {length}) => {
+		if(length > max) {
+			return length;
+		} else {
+			return max;
+		}
+	}, 0);
 
-                }
+	// Side bars
+	fortuneLines = fortuneLines.map((line, index, {length}) => {
+		const endPadding = " ".repeat(maxWidth - line.length);
+		// Handle fortune being single line differently
+		if(length === 1) {
+			return "< "  + line + endPadding + " >";
+		} else if(index === 0) {
+			return "/ "  + line + endPadding + " \\";
+		} else if(index === length - 1) {
+			return "\\ " + line + endPadding + " /";
+		} else {
+			return "| "  + line + endPadding + " |";
+		}
+	});
 
-                // Based on size of text, create top and bottom bars
-                // for textbox
-                for(var i = 0; i < maxWidth; ++i) {
-                    // Give bars some centering
-                    if(i === 0) {
-                        barsTop += "  ";
-                        barsBot += "  ";
-                    }
-                    barsTop += "_";
-                    barsBot += "-";
-                }
-            } while(textArray.join('').length === 0);
+	// Top and bottom bars, padded with space on the 2 sides
+	fortuneLines.unshift(" " + "_".repeat(maxWidth + 2));
+	fortuneLines.push(   " " + "-".repeat(maxWidth + 2));
 
-            cowsayTop.innerHTML = barsTop;
-            cowsayBot.innerHTML = barsBot;
-            cowsayOutput.innerHTML = textArray.join('');
-        }
-    })
-});
+	speechElement.textContent = fortuneLines.join("\n");
+})();
