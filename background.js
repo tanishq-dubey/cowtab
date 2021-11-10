b1218043059fb40888becdc1440cf73b6c38c1b9
Tanishq Dubey <tanishq.dubey@gmail.com>
2021-11-09 22:40:28 -0500

    add new weighting logic and upgrade manifest version

--- background.js
+++ background.js
@@ -1,19 +1,13 @@
 chrome.runtime.onInstalled.addListener(async ({reason, previousVersion}) => {
-	if(
-		   reason === "update"
-		&& previousVersion !== chrome.runtime.getManifest().version
-	) {
-		chrome.windows.create({url: "changes/changes.html", type: "popup"});
-	}
-
-	// Convert legacy option keys
 	{
 		const legacyOptions = {
 			bgColor: "backgroundColor",
 			fgColor: "textColor",
 			cowMods: "cowModifier"
 		};
+
 		const legacyKeys = Object.keys(legacyOptions);
+
 		chrome.storage.sync.get(legacyKeys, function(options) {
 			for(const [previous, current] of Object.entries(legacyOptions)) {
 				if(previous in options) {
@@ -24,17 +18,5 @@ chrome.runtime.onInstalled.addListener(async ({reason, previousVersion}) => {
 			chrome.storage.sync.remove(legacyKeys, ()=>{});
 			chrome.storage.sync.set(options, ()=>{});
 		})
-	}
-
-	// Save defaults options
-	{
-		const defaultOptions = {};
-		const optionsInfo = await fetchJSON("options/options.json");
-		for(const [option, data] of Object.entries(optionsInfo)) {
-			defaultOptions[option] = data.default;
-		}
-		await chrome.storage.sync.get(defaultOptions, async function(options) {
-			chrome.storage.sync.set(options, ()=>{});
-		});
 	}
 });
